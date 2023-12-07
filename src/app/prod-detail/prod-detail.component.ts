9143d693b7d61fe1fc77b8527c6142438157f839
Abhiraj <abhiraj.chaudhary@cloudeq.com>
2023-12-07 16:18:03 +0530

    commit

--- src/app/prod-detail/prod-detail.component.ts
+++ src/app/prod-detail/prod-detail.component.ts
@@ -4,14 +4,21 @@ import { ActivatedRoute } from '@angular/router';
 import { ProductService } from '../service/product.service';
 import { Product } from '../products';
 import { CommonModule } from '@angular/common';
+import { TruncatePipe } from '../truncate.pipe';
 
 @Component({
   selector: 'app-product-detail',
   standalone:true,
   templateUrl: './prod-detail.component.html',
-  imports: [CommonModule]
+  imports: [CommonModule,TruncatePipe]
 })
 export class ProductDetailComponent implements OnInit {
+addToCart(_t7: any) {
+throw new Error('Method not implemented.');
+}
+truncateDescription(arg0: any,arg1: number) {
+throw new Error('Method not implemented.');
+}
   product: Product[] = [];
 
   constructor(
--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -14,7 +14,7 @@ export class ProductService {
   constructor(private http: HttpClient) {}
 
   getProductById(productId: string): Observable<Response> {
-    const accessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOjE0LCJpYXQiOjE3MDE5MzU2OTUsImV4cCI6MTcwMTkzOTI5NX0.n3lPa77bG2x2acEtqSgZnMD_GpH8I1ejC-39ei9IX5M'; 
+    const accessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOjE0LCJpYXQiOjE3MDE5NDM1OTEsImV4cCI6MTcwMTk0NzE5MX0.8uGvJSrHkYM3BpccN9y_1wom21n3fD4of-dXgnIjkaE'; 
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
       Authorization: `${accessToken}`,
