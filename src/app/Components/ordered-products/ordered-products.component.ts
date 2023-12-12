71c138039984f7445ee1e064b692c810b036a28a
md-merajuddin-ceq <151141290+Merajuddin-ceq@users.noreply.github.com>
2023-12-12 15:38:29 +0530

    Add files via upload

--- src/app/Components/ordered-products/ordered-products.component.ts
+++ src/app/Components/ordered-products/ordered-products.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from "../nav-bar/nav-bar.component";
 import { FooterComponent } from "../footer/footer.component";
+import { ProductListComponent } from "./product-list/product-list.component";
 
 @Component({
     selector: 'app-ordered-products',
     standalone: true,
     templateUrl: './ordered-products.component.html',
-    imports: [CommonModule, NavBarComponent, FooterComponent]
+    styleUrl: './ordered-products.component.css',
+    imports: [CommonModule, NavBarComponent, FooterComponent, ProductListComponent]
 })
 export class OrderedProductsComponent {
 
