8df4fee717c9536ffbe5fd500defe3227f96727a
md-merajuddin-ceq <151141290+Merajuddin-ceq@users.noreply.github.com>
2023-12-14 15:19:07 +0530

    Add files via upload

--- src/app/Components/ordered-products/ordered-products.component.ts
+++ src/app/Components/ordered-products/ordered-products.component.ts
@@ -2,15 +2,17 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from "../nav-bar/nav-bar.component";
 import { FooterComponent } from "../footer/footer.component";
-import { ProductListComponent } from "./product-list/product-list.component";
+
 
 @Component({
     selector: 'app-ordered-products',
     standalone: true,
     templateUrl: './ordered-products.component.html',
     styleUrl: './ordered-products.component.css',
-    imports: [CommonModule, NavBarComponent, FooterComponent, ProductListComponent]
+    imports: [CommonModule, NavBarComponent, FooterComponent]
 })
 export class OrderedProductsComponent {
+
+    currentDate: Date = new Date();
 
 }
--- src/app/Components/profile/profile.component.ts
+++ src/app/Components/profile/profile.component.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavBarComponent } from "../nav-bar/nav-bar.component";
+import { FooterComponent } from "../footer/footer.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-profile',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './profile.component.html',
+    selector: 'app-profile',
+    standalone: true,
+    templateUrl: './profile.component.html',
+    styleUrl: './profile.component.css',
+    imports: [CommonModule, NavBarComponent, FooterComponent,FormsModule]
 })
 export class ProfileComponent {
+
+  showUpdateButton = false;
+
+  onClickOutside(event: MouseEvent): void {
+    const target = event.target as HTMLElement;
+    const isInputField = target.tagName === 'INPUT';
+
+  if (!isInputField) {
+    this.showUpdateButton = false;
+  }
+}
+
+  
+
+    profile = {timezone:'', firstname: 'xyz',lastname: 'xyz', phone: '987654321', email: 'john@example.com' };
+    editedProfile = { ...this.profile };
+    editMode = false;
+  
+    toggleEditMode(): void {
+      this.editMode = !this.editMode;
+    }
+  
+    saveChanges(): void {
+      // Apply changes to the profile, e.g., send data to the server
+      this.profile = { ...this.editedProfile };
+      this.toggleEditMode(); // Exit edit mode
+    }
+  
+    cancelEdit(): void {
+      this.editedProfile = { ...this.profile }; // Reset edited data
+      this.toggleEditMode(); // Exit edit mode
+    }
 
 }
