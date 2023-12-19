33ad375bc3f3739710eb5c226af409bd5ea97634
Abhiraj <abhiraj.chaudhary@cloudeq.com>
2023-12-19 18:39:45 +0530

    commit order

--- src/app/Components/ordered-products/orders/orders.component.ts
+++ src/app/Components/ordered-products/orders/orders.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GetOrdersService } from '../../../service/get-orders.service';
+import { TruncatePipe } from '../../../truncate.pipe';
 
 @Component({
   selector: 'app-orders',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,TruncatePipe],
   templateUrl: './orders.component.html',
 })
-export class OrdersComponent {
 
+export class OrdersComponent implements OnInit {
+  userId: string | null = null;
+  orders: any[] = [];
+
+  constructor(private getOrdersService: GetOrdersService) { }
+
+  ngOnInit(): void {
+    this.userId = localStorage.getItem("userId");
+    if (this.userId) {
+      this.fetchOrders(this.userId);
+    }
+  }
+
+  fetchOrders(userId: string): void {
+    this.getOrdersService.getOrders(userId).subscribe(
+      data => {
+        this.orders = data;
+      },
+      error => {
+        console.error('Error fetching orders:', error);
+      }
+    );
+  }
+  formatDate(dateString: string): string {
+    const date = new Date(dateString);
+    return date.toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric' });
+  }
+  
 }
+
+
--- src/app/service/get-orders.service.ts
+++ src/app/service/get-orders.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetOrdersService {
+  private url = 'http://172.31.1.135:8002/api/v1/orders/get-orders';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getOrders(userId: string): Observable<any> {
+    const accessToken = localStorage.getItem("access_token");
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    });
+
+    const payload = { userId: userId };
+
+    return this.http.post(this.url, payload, { headers: headers });
+  }
 }
+
